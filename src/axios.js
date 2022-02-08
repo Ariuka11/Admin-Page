56383c63f59516ae0d0f84c0a53d0ab1e5ba29d0
ariuka11 <ariun1118@gmail.com>
2022-02-08 12:47:09 -0500

    fetching data in home component

--- src/axios.js
+++ src/axios.js
@@ -3,14 +3,16 @@ import axios from "axios";
 const BASE_URL = "https://ecommerceapi-1.herokuapp.com/api/";
 
 const TOKEN =
-  localStorage.length > 1 &&
+  localStorage.length > 0 &&
   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
     .accessToken;
+
+console.log("TOKEN", TOKEN);
 export const request = axios.create({
   baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `bearer ${TOKEN}` },
+  headers: { token: `bearer ${TOKEN}` },
 });
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { userRequest } from "../../axios";
 import Chart from "../../components/chart/Chart";
 import Info from "../../components/info/Info";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
-import { userData } from "../../data";
 import "./home.css";
 const Home = () => {
+  const [userStat, setUserStat] = useState([]);
+  const months = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("stats");
+        res.data.map((item) => {
+          return setUserStat((prev) => [
+            ...prev,
+            { name: months[item._id - 1], "Active User": item.total },
+          ]);
+        });
+        console.log(userStat);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, []);
+
   return (
     <div className="home">
       <Info />
       <Chart
-        data={userData}
+        data={userStat}
         title="User Analytics"
         grid
         dataKey="Active User"
